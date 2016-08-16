764e96cde309230ab093fc2008138a0f2aed2161
Ben Ellison <ellison.ben@gmail.com>
2016-08-16 12:38:05 +0100

    Initial functions for tic tac toe

---
+++ tic_tac_toe.py
@@ -0,0 +1,27 @@
+
+def new_board():
+    b = 
+    return b
+
+def is_winner(player):
+
+def is_full(board):
+
+def is_valid(move):
+
+def display_board():
+
+def play_move(player, move):
+
+def change_player(player):
+    if player == 'X':
+        player = 'O'
+    else:
+        player = 'X'
+
+def tic_tac_toe():
+    board = new_board()
+    turns = 0
+
+if __name__ == '__main__':
+    tic_tac_toe()
